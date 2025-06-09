bad6469ed1369fe5c2927381ec041acf993c64b1
Ankush22004 <akushS0192@gmail.com>
2025-06-09 13:28:53 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("bharatverse-cache").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "dashboard.html",
+        "css/style.css",
+        "js/app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
